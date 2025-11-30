bbf76c65a83cf2d01d44b236c6cf89d50c470586
smit dhimar <smitdhimar61@gmail.com>
2025-11-30 18:38:52 -0500

    feat: give example for 'for', 'while', 'do-while' loops

--- src/core/loops/Loops.java
+++ src/core/loops/Loops.java
@@ -3,7 +3,19 @@ package core.loops;
 public class Loops {
     public static void main(String[] args){
         for(int i =0 ; i<5; i++){
-            System.out.println("Hello "+ i);
+            System.out.println("For loops: "+ i);
         }
+
+        int i = 0 ;
+        while(i < 5){
+            System.out.println("While loops: "+ i);
+            i++;
+        }
+        int j = 0;
+        do {
+            System.out.println("Do whiles loops: "+ j);
+            j++;
+        } while( j < 5);
+
     }
 }
