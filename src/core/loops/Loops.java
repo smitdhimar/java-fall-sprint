1d559006ee9e25f1cddeaa5d35ec0126aa9c4e23
smit dhimar <smitdhimar61@gmail.com>
2025-11-30 18:34:02 -0500

    feat: give an example for 'for' loops

---
+++ src/core/loops/Loops.java
@@ -0,0 +1,9 @@
+package core.loops;
+
+public class Loops {
+    public static void main(String[] args){
+        for(int i =0 ; i<5; i++){
+            System.out.println("Hello "+ i);
+        }
+    }
+}
