5cccb0ae5dd72c49df660d66f1a0e90a76e91528
smit dhimar <smitdhimar61@gmail.com>
2025-12-05 00:40:55 -0500

    feat: showcase of nThreads makes difference in execution time

--- src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
+++ src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
@@ -4,14 +4,33 @@ import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
+
         try {
-            ExecutorService executor = Executors.newFixedThreadPool(2);
 
-            for(int i = 1 ; i<10; i++){
-                executor.submit(()->{
+//            implement executor and autocloseable
+            ExecutorService executor = Executors.newFixedThreadPool(10);
 
+            long startTime = System.currentTimeMillis();
+
+            for (int i = 1; i < 10; i++) {
+                int taskNumber = i;
+
+                executor.submit(() -> {
+                    int fact = factorial(taskNumber);
+                    System.out.println("factorial of "+ taskNumber + " is "+ fact);
+                    return fact;
                 });
             }
+
+            executor.shutdown();
+            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+
+            while(!isComplete){
+                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+            }
+            long endTime = System.currentTimeMillis();
+
+            System.out.println("Time taken to complete is : "+ (endTime-startTime)/1000);
 
         } catch (Exception e) {
             System.out.print("FAILURE: Error in ExecutorServiceImplementation " + e);
