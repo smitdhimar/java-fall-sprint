d5fab5289d19f897f1261e237f925ffb55fb60b4
smit dhimar <smitdhimar61@gmail.com>
2025-12-05 23:11:45 -0500

    feat: implement invokeAll() function of ExecutorService instance

--- src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
+++ src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
@@ -3,6 +3,7 @@ package MultithreadingAndConcurrency.ExecutorServiceImplementation;
 import MultithreadingAndConcurrency.Utils.Factorial;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
@@ -12,24 +13,27 @@ public class Main {
 
 //            implement executor and autocloseable
             ExecutorService executor = Executors.newFixedThreadPool(2);
-
+            List<Callable<Integer>> tasks = new ArrayList<>();
             long startTime = System.currentTimeMillis();
             for (int i = 1; i < 10; i++) {
                 int taskNumber = i;
 
-                 executor.submit(() -> {
+                Callable<Integer> task = () -> {
                     int fact = Factorial.factorial(taskNumber);
                     System.out.println("factorial of " + taskNumber + " is " + fact);
                     return fact;
-                });
+                };
+                tasks.add(task);
             }
-
+            System.out.println("Unblocked");
+            executor.invokeAll(tasks);
+            System.out.println("blocked");
             executor.shutdown();
-            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-
-            while(!isComplete){
-                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-            }
+//            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//
+//            while (!isComplete) {
+//                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//            }
             long endTime = System.currentTimeMillis();
 
             System.out.println("Time taken to complete is : " + (endTime - startTime));
