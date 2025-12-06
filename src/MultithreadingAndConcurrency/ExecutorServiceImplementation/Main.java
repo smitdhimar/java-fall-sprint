69f1330d295005dd1fc357bfd0c7c159febe45d5
smit dhimar <smitdhimar61@gmail.com>
2025-12-05 19:29:11 -0500

    feat: implement futures and use get method to keep main thread awaiting

--- src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
+++ src/MultithreadingAndConcurrency/ExecutorServiceImplementation/Main.java
@@ -1,6 +1,8 @@
 package MultithreadingAndConcurrency.ExecutorServiceImplementation;
 
 import MultithreadingAndConcurrency.Utils.Factorial;
+
+import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class Main {
@@ -9,30 +11,41 @@ public class Main {
         try {
 
 //            implement executor and autocloseable
-            ExecutorService executor = Executors.newCachedThreadPool();
+            ExecutorService executor = Executors.newFixedThreadPool(2);
 
             long startTime = System.currentTimeMillis();
-
+            ArrayList<Future<Integer>> futures = new ArrayList<>();
             for (int i = 1; i < 10; i++) {
                 int taskNumber = i;
 
-                executor.submit(() -> {
+                Future<Integer> f = executor.submit(() -> {
                     int fact = Factorial.factorial(taskNumber);
-                    System.out.println("factorial of "+ taskNumber + " is "+ fact);
+                    System.out.println("factorial of " + taskNumber + " is " + fact);
                     return fact;
                 });
 
+                futures.add(f);
             }
 
+            for (Future<Integer> f : futures) {
+                try {
+                    Integer result = f.get();
+                    System.out.println("The result is : " + result);
+
+                } catch (Exception e) {
+                    System.out.println("FAILURE: error in futures " + e);
+                    e.printStackTrace();
+                }
+            }
             executor.shutdown();
-            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-
-            while(!isComplete){
-                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-            }
+//            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//
+//            while(!isComplete){
+//                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//            }
             long endTime = System.currentTimeMillis();
 
-            System.out.println("Time taken to complete is : "+ (endTime-startTime));
+            System.out.println("Time taken to complete is : " + (endTime - startTime));
 
         } catch (Exception e) {
             System.out.print("FAILURE: Error in ExecutorServiceImplementation " + e);
