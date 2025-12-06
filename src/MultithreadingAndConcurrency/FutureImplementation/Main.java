cd9b46005ce82dd7f6b100fe2e976f2614cd41fd
smit dhimar <smitdhimar61@gmail.com>
2025-12-05 23:06:42 -0500

    refactor: create seperate package for future implementation

--- src/MultithreadingAndConcurrency/FutureImplementation/Main.java
+++ src/MultithreadingAndConcurrency/FutureImplementation/Main.java
@@ -1,4 +1,4 @@
-package MultithreadingAndConcurrency.ExecutorServiceImplementation;
+package MultithreadingAndConcurrency.FutureImplementation;
 
 import MultithreadingAndConcurrency.Utils.Factorial;
 
@@ -14,22 +14,35 @@ public class Main {
             ExecutorService executor = Executors.newFixedThreadPool(2);
 
             long startTime = System.currentTimeMillis();
+            ArrayList<Future<Integer>> futures = new ArrayList<>();
             for (int i = 1; i < 10; i++) {
                 int taskNumber = i;
 
-                 executor.submit(() -> {
+                Future<Integer> f = executor.submit(() -> {
                     int fact = Factorial.factorial(taskNumber);
                     System.out.println("factorial of " + taskNumber + " is " + fact);
                     return fact;
                 });
+
+                futures.add(f);
             }
 
+            for (Future<Integer> f : futures) {
+                try {
+                    Integer result = f.get();
+                    System.out.println("The result is : " + result);
+
+                } catch (Exception e) {
+                    System.out.println("FAILURE: error in futures " + e);
+                    e.printStackTrace();
+                }
+            }
             executor.shutdown();
-            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-
-            while(!isComplete){
-                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
-            }
+//            boolean isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//
+//            while(!isComplete){
+//                isComplete = executor.awaitTermination(100, TimeUnit.MILLISECONDS);
+//            }
             long endTime = System.currentTimeMillis();
 
             System.out.println("Time taken to complete is : " + (endTime - startTime));
